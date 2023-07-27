2ccccb625a5c4bb72a9463d07f7c91919a5334c8
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-07-27 20:04:33 +0530

    Data Validation

--- src/CreditCardFraud/config/configuration.py
+++ src/CreditCardFraud/config/configuration.py
@@ -1,6 +1,7 @@
 from CreditCardFraud.constants import *
 from CreditCardFraud.utils.common import read_yaml , create_directories
-from CreditCardFraud.entity import (DataIngestionConfig)
+from CreditCardFraud.entity import (DataIngestionConfig,
+                                    DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -22,3 +23,17 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config
--- src/CreditCardFraud/entity/__init__.py
+++ src/CreditCardFraud/entity/__init__.py
@@ -4,4 +4,13 @@ from pathlib import Path
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir:Path
-    local_data_file:Path  
+    local_data_file:Path
+
+
+
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE:str
+    ALL_REQUIRED_FILES:list
