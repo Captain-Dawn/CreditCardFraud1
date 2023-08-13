8c6dd5a283d41e281832dcbae5693b070e7207b2
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2023-08-14 01:33:49 +0530

    prediction pipeline in progress

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask , render_template , request
+import os
+import numpy as np
+import pandas as pd
+
+from CreditCardFraud.pipeline.prediction import PredictionPipeline
+
+
+app = Flask(__name__)
+
+@app.route(rule='/',methods = ['GET'])
+def homePage():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
